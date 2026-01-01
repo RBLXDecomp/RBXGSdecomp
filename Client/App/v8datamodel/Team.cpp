4d62a7d3f80255ff0e19f9f117b4416687660836
Tad Walter <walterbrothersgaming@gmail.com>
2026-01-01 16:51:40 -0500

    Team and Teams

---
+++ Client/App/v8datamodel/Team.cpp
@@ -0,0 +1,53 @@
+#include "v8datamodel/Team.h"
+
+namespace RBX
+{
+	const char* sTeam = "Team";
+
+	static const Reflection::PropDescriptor<Team, int> prop_Score("Score", "Data", &Team::getScore, &Team::setScore, Reflection::PropertyDescriptor::STANDARD);
+	static const Reflection::PropDescriptor<Team, BrickColor> prop_Color("TeamColor", "Data", &Team::getTeamColor, &Team::setTeamColor, Reflection::PropertyDescriptor::STANDARD);
+	static const Reflection::PropDescriptor<Team, bool> prop_AutoAssignable("AutoAssignable", "Data", &Team::getAutoAssignable, &Team::setAutoAssignable, Reflection::PropertyDescriptor::STANDARD);
+
+	Team::Team()
+		: score(0),
+		  color(BrickColor::lego_194),
+		  autoAssignable(true),
+		  autoColorCharacters(true)
+	{
+		setName("Team");
+		color = BrickColor::lego_1;
+	}
+
+	int Team::getScore() const
+	{
+		return score;
+	}
+
+	void Team::setScore(int newScore)
+	{
+		score = newScore;
+		raisePropertyChanged(prop_Score);
+	}
+
+	BrickColor Team::getTeamColor() const
+	{
+		return color;
+	}
+
+	void Team::setTeamColor(BrickColor newColor)
+	{
+		color = newColor;
+		raisePropertyChanged(prop_Color);
+	}
+
+	bool Team::getAutoAssignable() const
+	{
+		return autoAssignable;
+	}
+
+	void Team::setAutoAssignable(bool value)
+	{
+		autoAssignable = value;
+		raisePropertyChanged(prop_AutoAssignable);
+	}
+}
---
+++ Client/App/v8datamodel/Teams.cpp
@@ -0,0 +1,107 @@
+#include "v8datamodel/Teams.h"
+#include "v8datamodel/ModelInstance.h"
+#include "Network/Players.h"
+
+namespace RBX
+{
+	const char* sTeams = "Teams";
+
+	Teams::Teams()
+	{
+		setName("Teams");
+	}
+
+	Team::~Team()
+	{
+	}
+
+	int Teams::getNumPlayersInTeam(BrickColor brickColor)
+	{
+		const ServiceProvider* provider = ServiceProvider::findServiceProvider(this);
+		Network::Players* players = provider->find<Network::Players>();
+		RBXASSERT(players);
+
+		int numOfPlayers = 0;
+		for (size_t i = 0; i < players->numChildren(); i++)
+		{
+			Network::Player* player = fastDynamicCast<Network::Player>(players->getChild(i));
+			if (player)
+			{
+				if (!player->getNeutral() && player->getTeamColor() == brickColor)
+					numOfPlayers++;
+			}
+		}
+		
+		return numOfPlayers;
+	}
+	
+	bool Teams::teamExists(BrickColor brickColor)
+	{
+		return getTeamFromTeamColor(brickColor) != NULL;
+	}
+
+	Team* Teams::getTeamFromTeamColor(BrickColor brickColor)
+	{
+		for (size_t i = 0; i < numChildren(); i++)
+		{
+			Team* team = fastDynamicCast<Team>(getChild(i));
+			if (team && team->getTeamColor() == brickColor)
+				return team;
+		}
+		
+		return NULL;
+	}
+
+	G3D::Color3 Teams::getTeamColorForHumanoid(Humanoid* humanoid)
+	{
+		const ServiceProvider* provider = ServiceProvider::findServiceProvider(this);
+		Network::Players* players = provider->find<Network::Players>();
+		RBXASSERT(players);
+
+		for (size_t i = 0; i < players->numChildren(); i++)
+		{
+			Network::Player* player = fastDynamicCast<Network::Player>(players->getChild(i));
+			if (player)
+			{
+				if (!player->getNeutral() && player->getCharacter())
+				{
+					ModelInstance* character = player->getCharacter();
+					if (character->findFirstChildOfType<Humanoid>() == humanoid)
+					{
+						return player->getTeamColor().color3();
+					}
+				}
+			}
+		}
+
+		return G3D::Color3::white();
+	}
+
+	void Teams::assignNewPlayerToTeam(Network::Player* player)
+	{
+		BrickColor currentTeam(BrickColor::lego_28);
+		bool foundTeam = false;
+
+		for (size_t i = 0; i < numChildren(); i++)
+		{
+			Team* team = fastDynamicCast<Team>(getChild(i));
+			if (team)
+			{
+				if (team->getAutoAssignable() == true)
+				{
+					if (getNumPlayersInTeam(team->getTeamColor()) < currentTeam.number)
+					{
+						currentTeam = team->getTeamColor();
+						foundTeam = true;
+					}
+				}
+			}
+		}
+		
+		if (foundTeam)
+		{
+			player->setTeamColor(currentTeam);
+			player->setNeutral(false);
+		}
+	}
+}
