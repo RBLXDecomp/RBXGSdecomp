93a3db77fefe974b1223cfe5a21a468364ff8ee5
luasoft10 <thatisanemail4741@gmail.com>
2026-03-01 19:49:48 +0200

    Players and misc. stuff (#114)
    * Players
    
    * what the dog doin
    
    * whitespace police
    
    * newlines
    
    * oops
    
    * a few more functions
    
    * a few more
    
    * chat, frontendProcessing
    
    * abuse reporting
    
    * move includes and declarations
    
    * OOPS
    
    * consistency
    
    * playerFromCharacter, getPlayerByID
    
    * oops
    
    * Context::current
    
    * weird

--- Client/App/include/reflection/property.h
+++ Client/App/include/reflection/property.h
@@ -138,8 +138,14 @@ namespace RBX
 			TypedPropertyDescriptor(ClassDescriptor&, const Type&, const char*, const char*, std::auto_ptr<GetSet>, Functionality);
 		public:
 			virtual bool isReadOnly() const;
-			PropType getValue(const DescribedBase*) const;
-			void setValue(DescribedBase*, const PropType&) const;
+			PropType getValue(const DescribedBase* object) const
+			{
+				return getset->getValue(object);
+			}
+			void setValue(DescribedBase* object, const PropType& value) const
+			{
+				getset->setValue(object, value);
+			}
 			virtual bool equalValues(const DescribedBase*, const DescribedBase*) const;
 			virtual bool hasStringValue() const;
 			virtual std::string getStringValue(const DescribedBase*) const;
